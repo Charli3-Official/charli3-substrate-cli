339d655b77a8c909da1698171d7fccb3c578bf86
stanislav-az <st.zhdanovich@gmail.com>
2025-08-28 13:48:44 +0500

    use sudo as

--- main.ts
+++ main.ts
@@ -33,7 +33,7 @@ async function main(): Promise<void> {
   const oracleCall = client.tx.oracle.sudoSetConfig(oracleConfig);
 
   // Wrap it in sudo.sudo
-  const sudoCall = client.tx.sudo.sudo(oracleCall.call);
+  const sudoCall = client.tx.sudo.sudoAs(alice.address, oracleCall.call);
 
   const unsub = await sudoCall.signAndSend(
     alice,
