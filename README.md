08faa9aacdded38796117d4d267f152ac7657108
stanislav-az <st.zhdanovich@gmail.com>
2025-09-03 20:32:26 +0500

    rm deno install nodejs and ts config

---
+++ README.md
@@ -0,0 +1,1 @@
+# substrate-submitter-ts
--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,30 @@
-import { DedotClient, WsProvider } from "dedot";
-import { u8aToHex } from "@polkadot/util";
-import { cryptoWaitReady } from "@polkadot/util-crypto";
-import { Keyring } from "@polkadot/keyring";
+import { DedotClient, WsProvider } from 'dedot';
+import { u8aToHex } from '@polkadot/util';
+import { cryptoWaitReady } from '@polkadot/util-crypto';
+import { Keyring } from '@polkadot/keyring';
 import type {
   Charli3SubstrateRuntimeApi,
   PalletOracleOracleConfiguration,
-} from "./charli3-substrate-runtime/index.d.ts";
+} from './charli3-substrate-runtime/index.js';
 
 async function main(): Promise<void> {
   // Connect
-  const provider = new WsProvider("ws://127.0.0.1:9944");
+  const provider = new WsProvider('ws://127.0.0.1:9944');
   const client = await DedotClient.new<Charli3SubstrateRuntimeApi>(provider);
 
   // Load an Ed25519 keypair
   await cryptoWaitReady();
-  const keyring = new Keyring({ type: "ed25519" });
+  const keyring = new Keyring({ type: 'ed25519' });
   const alice = keyring.addFromUri(
-    "bottom drive obey lake curtain smoke basket hold race lonely fit walk//Alice",
+    'bottom drive obey lake curtain smoke basket hold race lonely fit walk//Alice',
   );
-  console.log("Alice pk", u8aToHex(alice.publicKey));
+  console.log('Alice pk', u8aToHex(alice.publicKey));
   const sudoKey = await client.query.sudo.key();
-  console.log("Sudo key is  ", sudoKey?.address() ?? "Not found");
-  console.log("Alice account", alice.address);
+  console.log('Sudo key is  ', sudoKey?.address() ?? 'Not found');
+  console.log('Alice account', alice.address);
 
   let oracleConfig = await getCurrentConfig(client);
-  console.log("Oracle config:", oracleConfig);
+  console.log('Oracle config:', oracleConfig);
   oracleConfig.minNodesForTrustedAggregation = 3;
   oracleConfig.feedAge = 12;
   oracleConfig.outliersRange = 160;
@@ -40,20 +40,18 @@ async function main(): Promise<void> {
     alice,
     { tip: 0n },
     async ({ status, dispatchError }) => {
-      console.log("Transaction status", status.type);
+      console.log('Transaction status', status.type);
       if (dispatchError) {
-        console.log("Dispatch error:", dispatchError.type);
-        if (dispatchError.type === "Module") {
-          console.log("Dispatch module:", dispatchError.value);
+        console.log('Dispatch error:', dispatchError.type);
+        if (dispatchError.type === 'Module') {
+          console.log('Dispatch module:', dispatchError.value);
         }
       }
-      if (status.type === "BestChainBlockIncluded") {
+      if (status.type === 'BestChainBlockIncluded') {
         console.log(`Transaction is included in best block`);
       }
-      if (status.type === "Finalized") {
-        console.log(
-          `Transaction finalized at block hash ${status.value.blockHash}`,
-        );
+      if (status.type === 'Finalized') {
+        console.log(`Transaction finalized at block hash ${status.value.blockHash}`);
         await unsub();
       }
     },
@@ -61,12 +59,10 @@ async function main(): Promise<void> {
 
   // Query config
   const waitTime = Number(client.consts.aura.slotDuration) + 1_000;
-  console.log(
-    `Waiting for ${waitTime} milliseconds (block production time + 1 sec)...`,
-  );
+  console.log(`Waiting for ${waitTime} milliseconds (block production time + 1 sec)...`);
   await new Promise((resolve) => setTimeout(resolve, waitTime));
   oracleConfig = await getCurrentConfig(client);
-  console.log("Oracle config:", oracleConfig);
+  console.log('Oracle config:', oracleConfig);
 
   // Disconnect
   await provider.disconnect();
@@ -77,8 +73,7 @@ main().catch(console.error);
 async function getCurrentConfig(
   client: DedotClient<Charli3SubstrateRuntimeApi>,
 ): Promise<PalletOracleOracleConfiguration> {
-  const minNodesForTrustedAggregation = await client.query.oracle
-    .minNodesForTrustedAggregation();
+  const minNodesForTrustedAggregation = await client.query.oracle.minNodesForTrustedAggregation();
   const feedAge = await client.query.oracle.feedAge();
   const outliersRange = await client.query.oracle.outliersRange();
   const divergency = await client.query.oracle.divergency();
