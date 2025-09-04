0c3f20697ad0e53fd98f034f17f8a099880ca8b5
stanislav-az <st.zhdanovich@gmail.com>
2025-09-04 20:44:03 +0500

    refactor substrate query and crypto

---
+++ src/crypto.ts
@@ -0,0 +1,58 @@
+import { BN, u8aToHex } from '@polkadot/util';
+import { createKeyMulti, encodeAddress, cryptoWaitReady } from '@polkadot/util-crypto';
+import { Keyring } from '@polkadot/keyring';
+import type { KeyringPair } from '@polkadot/keyring/types';
+
+export type { TestnetWallets };
+export { loadTestnetWallets, createMultiAddress };
+
+interface TestnetWallets {
+  readonly alice: KeyringPair;
+  readonly bob: KeyringPair;
+  readonly charlie: KeyringPair;
+  readonly dave: KeyringPair;
+  readonly eve: KeyringPair;
+}
+
+async function loadTestnetWallets(): Promise<TestnetWallets> {
+  await cryptoWaitReady();
+  const keyring = new Keyring({ type: 'ed25519' });
+  const alice = keyring.addFromUri(
+    'bottom drive obey lake curtain smoke basket hold race lonely fit walk//Alice',
+  );
+  console.log('Alice pk  ', u8aToHex(alice.publicKey));
+  const bob = keyring.addFromUri(
+    'bottom drive obey lake curtain smoke basket hold race lonely fit walk//Bob',
+  );
+  console.log('Bob pk    ', u8aToHex(bob.publicKey));
+  const charlie = keyring.addFromUri(
+    'bottom drive obey lake curtain smoke basket hold race lonely fit walk//Charlie',
+  );
+  console.log('Charlie pk', u8aToHex(charlie.publicKey));
+  const dave = keyring.addFromUri(
+    'bottom drive obey lake curtain smoke basket hold race lonely fit walk//Dave',
+  );
+  console.log('Dave pk', u8aToHex(dave.publicKey));
+  const eve = keyring.addFromUri(
+    'bottom drive obey lake curtain smoke basket hold race lonely fit walk//Eve',
+  );
+  console.log('Eve pk', u8aToHex(eve.publicKey));
+
+  return {
+    alice,
+    bob,
+    charlie,
+    dave,
+    eve,
+  };
+}
+
+function createMultiAddress(
+  signers: (string | Uint8Array<ArrayBufferLike>)[],
+  threshold: bigint | BN | number,
+): string {
+  const multiPub = createKeyMulti(signers, threshold);
+  const multiAddr = encodeAddress(multiPub, 42);
+  console.log('Multisig address', multiAddr);
+  return multiAddr;
+}
---
+++ src/query.ts
@@ -0,0 +1,51 @@
+import { DedotClient, WsProvider } from 'dedot';
+
+import type {
+  Charli3SubstrateRuntimeApi,
+  PalletOracleOracleConfiguration,
+} from './charli3-substrate-runtime/index.js';
+
+export { getCurrentConfig, useSubstrateClient };
+
+async function getCurrentConfig(
+  client: DedotClient<Charli3SubstrateRuntimeApi>,
+): Promise<PalletOracleOracleConfiguration> {
+  const minNodesForTrustedAggregation = await client.query.oracle.minNodesForTrustedAggregation();
+  const feedAge = await client.query.oracle.feedAge();
+  const outliersRange = await client.query.oracle.outliersRange();
+  const divergency = await client.query.oracle.divergency();
+  const tradePairs = await client.query.oracle.tradePairs();
+  if (
+    minNodesForTrustedAggregation === undefined ||
+    feedAge === undefined ||
+    outliersRange === undefined ||
+    divergency === undefined ||
+    tradePairs === undefined
+  ) {
+    throw new Error("Couldn't load oracle config");
+  }
+
+  return {
+    minNodesForTrustedAggregation,
+    feedAge,
+    outliersRange,
+    divergency,
+    tradePairs,
+  };
+}
+
+async function useSubstrateClient(
+  action: (client: DedotClient<Charli3SubstrateRuntimeApi>) => Promise<void>,
+) {
+  let provider;
+  try {
+    // Connect
+    provider = new WsProvider('ws://127.0.0.1:9944');
+    const client = await DedotClient.new<Charli3SubstrateRuntimeApi>(provider);
+    // Use
+    await action(client);
+  } finally {
+    // Disconnect
+    if (provider) await provider.disconnect();
+  }
+}
