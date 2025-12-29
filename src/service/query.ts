be457d8882de5e83735ef02ba82c9c7859c361e4
stanislav-az <st.zhdanovich@gmail.com>
2025-12-29 20:43:02 +0500

    add substrate rpc url selection

--- src/service/query.ts
+++ src/service/query.ts
@@ -48,14 +48,14 @@ async function getCurrentConfig(
   };
 }
 
-// TODO wsJsonRpcUrl: string
 async function useSubstrateClient(
+  wsJsonRpcUrl: string,
   action: (client: DedotClient<Charli3SubstrateRuntimeApi>) => Promise<void>,
 ) {
   let provider;
   try {
     // Connect
-    provider = new WsProvider('ws://127.0.0.1:9944');
+    provider = new WsProvider(wsJsonRpcUrl);
     const client = await DedotClient.new<Charli3SubstrateRuntimeApi>(provider);
     // Use
     await action(client);
