81e56e67be52a534142c2edf20e49aee7744d6e6
stanislav-az <st.zhdanovich@gmail.com>
2025-12-30 13:26:07 +0500

    add multisig to add and del node

--- src/cli.ts
+++ src/cli.ts
@@ -156,14 +156,24 @@ async function signConfigUpdate(
   });
 }
 
-async function authorizeNode(wallet: string, nodePubKey: string, wsJsonRpcUrl: string) {
+async function startAuthorizeNode(
+  wallet: string,
+  configPath: string,
+  nodePubKey: string,
+  wsJsonRpcUrl: string,
+) {
   // Select wallet
   const thisWallet = await selectWallet(wallet);
+
+  // Load multisig and oracle config from YAML
+  const { multisig } = loadCliConfig(configPath);
 
   const nodeKey = new AccountId32(nodePubKey);
   console.log('Node key is ', nodeKey);
 
   await useSubstrateClient(wsJsonRpcUrl, async (client) => {
+    const multiAddr = createMultiAddress(multisig.addresses, multisig.threshold);
+
     const sudoKey = await client.query.sudo.key();
     console.log('Sudo address is ', sudoKey?.address() ?? 'Not found');
 
@@ -174,7 +184,124 @@ async function authorizeNode(wallet: string, nodePubKey: string, wsJsonRpcUrl: s
     const oracleCall = client.tx.oracle.sudoRegisterOracleNode(nodeKey);
     // Wrap it in sudo.sudo
     const sudoCall = client.tx.sudo.sudo(oracleCall.call);
-    const unsub = await sudoCall.signAndSend(thisWallet, { tip: 0n }, async (result) => {
+
+    // Wrap it in multisig
+    const otherSignatories = sortAddresses(
+      multisig.addresses.filter((value) => value !== thisWallet.address),
+      42,
+    );
+    const txMulti = client.tx.multisig.asMulti(
+      multisig.threshold,
+      otherSignatories,
+      undefined,
+      sudoCall.call,
+      {
+        refTime: 0n,
+        proofSize: 0n,
+      },
+    );
+    const txEstimation = await txMulti.paymentInfo(thisWallet, { tip: 0n });
+    console.log('tx estimation', txEstimation);
+    const safeWeight = {
+      refTime: txEstimation.weight.refTime * 2n,
+      proofSize: txEstimation.weight.proofSize * 2n,
+    };
+    const txFinal = client.tx.multisig.asMulti(
+      multisig.threshold,
+      otherSignatories,
+      undefined,
+      sudoCall.call,
+      safeWeight,
+    );
+    let txMultiHash: `0x${string}` | undefined = undefined;
+    const unsub = await txFinal.signAndSend(thisWallet, { tip: 0n }, async (result) => {
+      txCallback(result);
+      for (const e of result.events) {
+        if (e.event.pallet === 'Multisig' && e.event.palletEvent.name === 'NewMultisig') {
+          console.log(e.event.palletEvent.data);
+          txMultiHash = e.event.palletEvent.data.callHash;
+          console.log('tx id', txMultiHash);
+        }
+      }
+      if (result.status.type === 'Finalized') await unsub();
+    });
+    await waitForTx(client);
+    if (!txMultiHash) throw new Error('Was not able to submit new multisig');
+    const multisigTx = await client.query.multisig.multisigs([multiAddr, txMultiHash]);
+    console.log('multisig tx stored', multisigTx);
+
+    // Query result
+    const updatedNodesRaw = await client.query.oracle.authorizedOracleNodes.entries();
+    const updatedNodes = updatedNodesRaw.map((account) => account[0]);
+    console.log('Updated oracle nodes:', updatedNodes);
+    if (updatedNodes.find((el) => el.eq(nodeKey))) {
+      console.log('Oracle node added successfully!');
+    }
+  });
+}
+
+async function signAuthorizeNode(
+  wallet: string,
+  multisigStartTxId: Bytes,
+  configPath: string,
+  nodePubKey: string,
+  wsJsonRpcUrl: string,
+) {
+  // Select wallet
+  const thisWallet = await selectWallet(wallet);
+
+  // Load multisig and oracle config from YAML
+  const { multisig } = loadCliConfig(configPath);
+
+  const nodeKey = new AccountId32(nodePubKey);
+  console.log('Node key is ', nodeKey);
+
+  await useSubstrateClient(wsJsonRpcUrl, async (client) => {
+    const multiAddr = createMultiAddress(multisig.addresses, multisig.threshold);
+
+    const sudoKey = await client.query.sudo.key();
+    console.log('Sudo address is ', sudoKey?.address() ?? 'Not found');
+
+    const currentNodesRaw = await client.query.oracle.authorizedOracleNodes.entries();
+    const currentNodes = currentNodesRaw.map((account) => account[0]);
+    console.log('Current oracle nodes:', currentNodes);
+
+    const oracleCall = client.tx.oracle.sudoRegisterOracleNode(nodeKey);
+    // Wrap it in sudo.sudo
+    const sudoCall = client.tx.sudo.sudo(oracleCall.call);
+
+    const multisigStartTx = await client.query.multisig.multisigs([multiAddr, multisigStartTxId]);
+    if (!multisigStartTx) throw new Error('Was not able to retrieve multisig');
+
+    // Wrap it in multisig
+    const otherSignatories = sortAddresses(
+      multisig.addresses.filter((value) => value !== thisWallet.address),
+      42,
+    );
+    const txMulti = client.tx.multisig.asMulti(
+      multisig.threshold,
+      otherSignatories,
+      multisigStartTx.when,
+      sudoCall.call,
+      {
+        refTime: 0n,
+        proofSize: 0n,
+      },
+    );
+    const txEstimation = await txMulti.paymentInfo(thisWallet, { tip: 0n });
+    console.log('tx estimation', txEstimation);
+    const safeWeight = {
+      refTime: txEstimation.weight.refTime * 2n,
+      proofSize: txEstimation.weight.proofSize * 2n,
+    };
+    const txFinal = client.tx.multisig.asMulti(
+      multisig.threshold,
+      otherSignatories,
+      multisigStartTx.when,
+      sudoCall.call,
+      safeWeight,
+    );
+    const unsub = await txFinal.signAndSend(thisWallet, { tip: 0n }, async (result) => {
       txCallback(result);
       if (result.status.type === 'Finalized') await unsub();
     });
@@ -190,14 +317,24 @@ async function authorizeNode(wallet: string, nodePubKey: string, wsJsonRpcUrl: s
   });
 }
 
-async function deauthorizeNode(wallet: string, nodePubKey: string, wsJsonRpcUrl: string) {
+async function startDeauthorizeNode(
+  wallet: string,
+  configPath: string,
+  nodePubKey: string,
+  wsJsonRpcUrl: string,
+) {
   // Select wallet
   const thisWallet = await selectWallet(wallet);
+
+  // Load multisig and oracle config from YAML
+  const { multisig } = loadCliConfig(configPath);
 
   const nodeKey = new AccountId32(nodePubKey);
   console.log('Node key is ', nodeKey);
 
   await useSubstrateClient(wsJsonRpcUrl, async (client) => {
+    const multiAddr = createMultiAddress(multisig.addresses, multisig.threshold);
+
     const sudoKey = await client.query.sudo.key();
     console.log('Sudo address is ', sudoKey?.address() ?? 'Not found');
 
@@ -208,7 +345,124 @@ async function deauthorizeNode(wallet: string, nodePubKey: string, wsJsonRpcUrl:
     const oracleCall = client.tx.oracle.sudoDeregisterOracleNode(nodeKey);
     // Wrap it in sudo.sudo
     const sudoCall = client.tx.sudo.sudo(oracleCall.call);
-    const unsub = await sudoCall.signAndSend(thisWallet, { tip: 0n }, async (result) => {
+
+    // Wrap it in multisig
+    const otherSignatories = sortAddresses(
+      multisig.addresses.filter((value) => value !== thisWallet.address),
+      42,
+    );
+    const txMulti = client.tx.multisig.asMulti(
+      multisig.threshold,
+      otherSignatories,
+      undefined,
+      sudoCall.call,
+      {
+        refTime: 0n,
+        proofSize: 0n,
+      },
+    );
+    const txEstimation = await txMulti.paymentInfo(thisWallet, { tip: 0n });
+    console.log('tx estimation', txEstimation);
+    const safeWeight = {
+      refTime: txEstimation.weight.refTime * 2n,
+      proofSize: txEstimation.weight.proofSize * 2n,
+    };
+    const txFinal = client.tx.multisig.asMulti(
+      multisig.threshold,
+      otherSignatories,
+      undefined,
+      sudoCall.call,
+      safeWeight,
+    );
+    let txMultiHash: `0x${string}` | undefined = undefined;
+    const unsub = await txFinal.signAndSend(thisWallet, { tip: 0n }, async (result) => {
+      txCallback(result);
+      for (const e of result.events) {
+        if (e.event.pallet === 'Multisig' && e.event.palletEvent.name === 'NewMultisig') {
+          console.log(e.event.palletEvent.data);
+          txMultiHash = e.event.palletEvent.data.callHash;
+          console.log('tx id', txMultiHash);
+        }
+      }
+      if (result.status.type === 'Finalized') await unsub();
+    });
+    await waitForTx(client);
+    if (!txMultiHash) throw new Error('Was not able to submit new multisig');
+    const multisigTx = await client.query.multisig.multisigs([multiAddr, txMultiHash]);
+    console.log('multisig tx stored', multisigTx);
+
+    // Query result
+    const updatedNodesRaw = await client.query.oracle.authorizedOracleNodes.entries();
+    const updatedNodes = updatedNodesRaw.map((account) => account[0]);
+    console.log('Updated oracle nodes:', updatedNodes);
+    if (updatedNodes.find((el) => el.eq(nodeKey)) === undefined) {
+      console.log('Oracle node removed successfully!');
+    }
+  });
+}
+
+async function signDeauthorizeNode(
+  wallet: string,
+  multisigStartTxId: Bytes,
+  configPath: string,
+  nodePubKey: string,
+  wsJsonRpcUrl: string,
+) {
+  // Select wallet
+  const thisWallet = await selectWallet(wallet);
+
+  // Load multisig and oracle config from YAML
+  const { multisig } = loadCliConfig(configPath);
+
+  const nodeKey = new AccountId32(nodePubKey);
+  console.log('Node key is ', nodeKey);
+
+  await useSubstrateClient(wsJsonRpcUrl, async (client) => {
+    const multiAddr = createMultiAddress(multisig.addresses, multisig.threshold);
+
+    const sudoKey = await client.query.sudo.key();
+    console.log('Sudo address is ', sudoKey?.address() ?? 'Not found');
+
+    const currentNodesRaw = await client.query.oracle.authorizedOracleNodes.entries();
+    const currentNodes = currentNodesRaw.map((account) => account[0]);
+    console.log('Current oracle nodes:', currentNodes);
+
+    const oracleCall = client.tx.oracle.sudoDeregisterOracleNode(nodeKey);
+    // Wrap it in sudo.sudo
+    const sudoCall = client.tx.sudo.sudo(oracleCall.call);
+
+    const multisigStartTx = await client.query.multisig.multisigs([multiAddr, multisigStartTxId]);
+    if (!multisigStartTx) throw new Error('Was not able to retrieve multisig');
+
+    // Wrap it in multisig
+    const otherSignatories = sortAddresses(
+      multisig.addresses.filter((value) => value !== thisWallet.address),
+      42,
+    );
+    const txMulti = client.tx.multisig.asMulti(
+      multisig.threshold,
+      otherSignatories,
+      multisigStartTx.when,
+      sudoCall.call,
+      {
+        refTime: 0n,
+        proofSize: 0n,
+      },
+    );
+    const txEstimation = await txMulti.paymentInfo(thisWallet, { tip: 0n });
+    console.log('tx estimation', txEstimation);
+    const safeWeight = {
+      refTime: txEstimation.weight.refTime * 2n,
+      proofSize: txEstimation.weight.proofSize * 2n,
+    };
+    const txFinal = client.tx.multisig.asMulti(
+      multisig.threshold,
+      otherSignatories,
+      multisigStartTx.when,
+      sudoCall.call,
+      safeWeight,
+    );
+    const unsub = await txFinal.signAndSend(thisWallet, { tip: 0n }, async (result) => {
       txCallback(result);
       if (result.status.type === 'Finalized') await unsub();
     });
@@ -284,11 +538,16 @@ program
   });
 
 program
-  .command('authorize-node')
-  .description('Authorize Oracle Node')
+  .command('start-authorize-node')
+  .description('Submit new Authorize Oracle Node tx')
   .requiredOption(
     '-w, --wallet <string>',
     'String refers to wallet test name, e.g. Alice, or the suri itself',
+  )
+  .option(
+    '-c, --config <path>',
+    'YAML file for Multisig and Oracle configurations',
+    'testnet-config.yml',
   )
   .requiredOption('-n, --node <string>', 'String refers to AccountId32Like of added node')
   .option(
@@ -297,15 +556,43 @@ program
     'ws://127.0.0.1:9944',
   )
   .action(async (opts) => {
-    await authorizeNode(opts.wallet, opts.node, opts.substrateRpc);
+    await startAuthorizeNode(opts.wallet, opts.config, opts.node, opts.substrateRpc);
   });
 
 program
-  .command('deauthorize-node')
-  .description('Deauthorize Oracle Node')
+  .command('sign-authorize-node')
+  .description('Sign Authorize Oracle Node tx')
   .requiredOption(
     '-w, --wallet <string>',
     'String refers to wallet test name, e.g. Alice, or the suri itself',
+  )
+  .requiredOption('-x, --tx <id>', 'Original multisig start tx hash as an 0x-string')
+  .option(
+    '-c, --config <path>',
+    'YAML file for Multisig and Oracle configurations',
+    'testnet-config.yml',
+  )
+  .requiredOption('-n, --node <string>', 'String refers to AccountId32Like of added node')
+  .option(
+    '-s, --substrate-rpc <url>',
+    'Web Socket URL for JSON RPC protocol',
+    'ws://127.0.0.1:9944',
+  )
+  .action(async (opts) => {
+    await signAuthorizeNode(opts.wallet, opts.tx, opts.config, opts.node, opts.substrateRpc);
+  });
+
+program
+  .command('start-deauthorize-node')
+  .description('Submit new Deauthorize Oracle Node tx')
+  .requiredOption(
+    '-w, --wallet <string>',
+    'String refers to wallet test name, e.g. Alice, or the suri itself',
+  )
+  .option(
+    '-c, --config <path>',
+    'YAML file for Multisig and Oracle configurations',
+    'testnet-config.yml',
   )
   .requiredOption('-n, --node <string>', 'String refers to AccountId32Like of removed node')
   .option(
@@ -314,7 +601,30 @@ program
     'ws://127.0.0.1:9944',
   )
   .action(async (opts) => {
-    await deauthorizeNode(opts.wallet, opts.node, opts.substrateRpc);
+    await startDeauthorizeNode(opts.wallet, opts.config, opts.node, opts.substrateRpc);
+  });
+
+program
+  .command('sign-deauthorize-node')
+  .description('Sign Deauthorize Oracle Node tx')
+  .requiredOption(
+    '-w, --wallet <string>',
+    'String refers to wallet test name, e.g. Alice, or the suri itself',
+  )
+  .requiredOption('-x, --tx <id>', 'Original multisig start tx hash as an 0x-string')
+  .option(
+    '-c, --config <path>',
+    'YAML file for Multisig and Oracle configurations',
+    'testnet-config.yml',
+  )
+  .requiredOption('-n, --node <string>', 'String refers to AccountId32Like of added node')
+  .option(
+    '-s, --substrate-rpc <url>',
+    'Web Socket URL for JSON RPC protocol',
+    'ws://127.0.0.1:9944',
+  )
+  .action(async (opts) => {
+    await signDeauthorizeNode(opts.wallet, opts.tx, opts.config, opts.node, opts.substrateRpc);
   });
 
 program.parseAsync(process.argv);
